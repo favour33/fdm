6a60f24b512dfb730c53a9641b9f76e0e0356a04
Favour Nwachukwu <fnwachukwu84@gmail.com>
2022-03-21 19:57:18 +0000

    Added Basic employee form page

--- src/Pages/Employee/Employees.js
+++ src/Pages/Employee/Employees.js
@@ -1,7 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Employees = () => {
-  return <div>Employees</div>;
+  const claimList = {
+    name: "",
+    desritpon: "",
+    amount: "",
+    evidence: "",
+    date: "",
+    stats: "",
+  };
+
+  const [name, setName] = useState("");
+  const [desritpon, setDesritpon] = useState("");
+  const [amount, setAmount] = useState("");
+  const [evidence, setEvidence] = useState("");
+  const [date, setDate] = useState("");
+  const [stats, setStats] = useState("PENDING");
+
+  return (
+    <>
+      <h1>Add A Claim </h1>
+      <form>
+        <label htmlFor="name">Claim Name</label>
+        <input
+          type="text"
+          id="name"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          value={name}
+        />
+        <hr />
+        <label htmlFor="desc">Claim Name</label>
+        <textarea
+          id="desc"
+          onChange={(e) => {
+            setDesritpon(e.target.value);
+          }}
+          value={desritpon}
+        ></textarea>
+        <hr />
+        <label htmlFor="amount">Claim Amount</label>
+        <input
+          type="text"
+          id="amount"
+          onChange={(e) => {
+            setAmount(e.target.value);
+          }}
+          value={amount}
+        />
+        <hr />
+        {/* <input type="file" /> */}
+        <label htmlFor="image">Add Image</label>
+        <input
+          type="text"
+          id="image"
+          onChange={(e) => {
+            setEvidence(e.target.value);
+          }}
+          value={evidence}
+        />
+        <hr />
+        <input
+          type="date"
+          onChange={(e) => {
+            setDate(e.target.value);
+          }}
+          value={date}
+        />
+        <hr />
+        <button>ADD CLAIM</button>
+      </form>
+      <h1>All sent Claims </h1>
+      <p>Name: {name}</p>
+      <p>desritpon: {desritpon}</p>
+      <p>amount: {amount}</p>
+      <p>evidence: {evidence}</p>
+      <p>date: {date}</p>
+      <p>status: {stats}</p>
+    </>
+  );
 };
 
 export default Employees;
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
-import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "../Pages/Login/Login";
+import Employees from "../Pages/Employee/Employees";
+import Manager from "../Pages/Manger/Manager";
+import Finance from "../Pages/FianceDepartment/FinanceDepartment";
+import NotFound from "../Pages/NotFound/NotFound";
 
 const App = () => {
-  // const adminUser = {
-  //   email: "",
-  //   password,
-  // };
   return (
     <>
-      <h1>Expense Application</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/employees" element={<Employees />} />
+          <Route path="/managers" element={<Manager />} />
+          <Route path="/finances" element={<Finance />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
