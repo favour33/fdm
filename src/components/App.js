5f2de02f1d5eb3b49101a20c2a180ac02f2da4bc
Favour Nwachukwu <fnwachukwu84@gmail.com>
2022-03-21 13:26:23 +0000

    Project Set-up

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+const App = () => {
+  // const adminUser = {
+  //   email: "",
+  //   password,
+  // };
+  return (
+    <>
+      <h1>Expense Application</h1>
+    </>
+  );
+};
+
+export default App;
